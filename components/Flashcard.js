415e41080e74b0a64e7835f95a75186f678e3cb7
Leigh <leighlawhon@gmail.com>
2017-04-30 06:42:46 -0400

    adding terms ot card

--- components/Flashcard.js
+++ components/Flashcard.js
@@ -10,26 +10,35 @@ import {
 export default class Flashcard extends Component {
   constructor(props) {
     super(props);
-    // console.log(props, 'p questions');
-    // this.state = {
-    //   questions: new ListView.DataSource({
-    //     rowHasChanged: (row1, row2) => row1 !== row2,
-    //   }),
-    //   loaded: false,
-    // };
+    console.log(props, 'p questions');
+    this.state = {
+      questions: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
   }
-  // componentDidMount(){
-  //   console.log(this.props, 'component mounted');
-  //   const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-  //   this.setState({
-  //     questions: ds.cloneWithRows(this.props.questions)
-  //   })
-  // }
+  componentDidMount(){
+    console.log(this.state, 'component mounted');
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.setState({
+      questions: ds.cloneWithRows(this.props.questions)
+    })
+  }
   render() {
-    console.log(this.props, "FC render");
+    console.log(this.state, "FC render");
     const styles= this.props.styles;
     return (
-      <Text/>
+      <ListView
+        dataSource={this.state.questions}
+        style={styles.listView}
+        renderRow={
+          (q) =>
+          <View style={styles.container}>
+            <Text>{q.term}</Text>          
+          </View>
+        }
+      />
     );
   }
 }
